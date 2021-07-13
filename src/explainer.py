94579634314eaceeeaa43609f5fc235c6bb027de
ah96 <amarhalilovic96@gmail.com>
2021-07-13 15:50:30 +0200

    Upgraded explainer.py and DataLoader.py

--- src/explainer.py
+++ src/explainer.py
@@ -6,22 +6,21 @@
 # possible explanation modes: 'tabular', 'image', 'tabular_costmap'
 explanationMode = 'image'
 
-# possible (tabular) modes: 'regression', 'classification'
+# possible (tabular explanation) modes: 'regression', 'classification'
 mode = 'regression'
 
-# one hot encoding - needed for LIME tabular classification
+# one hot encoding: True or False - needed for LIME tabular classification
 one_hot_encoding = True
 
 # header of the output class/column
 output_class_name = 'beginning' # just to differ if it was changed properly
 
-# choose number of samples
+# set number of samples
 num_samples = 256
 
 
 
 # Data loading
-
 from lime_explainer import DataLoader
 
 # load output data
@@ -34,73 +33,69 @@ odom, plan, teb_global_plan, teb_local_plan, current_goal, local_costmap_data, l
 
 #print("input loaded")
 
-# Izbaciti unose sa 'None' frejmom
+# Delete entries with 'None' frame
 
-# Detektuj broj unosa sa 'None' frejmom na osnovu local_costmap_info
+# Detect number of entries with 'None' frame based on local_costmap_info
 numOfFirstRowsToDelete = len(local_costmap_info[local_costmap_info['frame'] == 'None'])
 #print(numOfFirstRowsToDelete)
 
-# Izbaci unose iz local_costmap_info
+# Delete entries with 'None' frame from local_costmap_info
 local_costmap_info.drop(index=local_costmap_info.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
 #print(local_costmap_info)
 
-# Izbaci unose iz odom
+# Delete entries with 'None' frame from odom
 odom.drop(index=odom.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
 #print(odom)
 
-# Izbaci unose iz tf_odom_map
-tf_odom_map.drop(index=tf_odom_map.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
-#print(tf_odom_map)
-
-# Izbaci unose iz tf_map_odom
-tf_map_odom.drop(index=tf_map_odom.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
-#print(tf_map_odom)
-
-# Izbaci unose iz amcl_pose
+# Delete entries with 'None' frame from amcl_pose
 amcl_pose.drop(index=amcl_pose.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
 #print(amcl_pose)
 
-# Izbaci unose iz cmd_vel
+# Delete entries with 'None' frame from cmd_vel
 cmd_vel.drop(index=cmd_vel.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
 #print(cmd_vel)
 
+# Delete entries with 'None' frame from tf_odom_map
+tf_odom_map.drop(index=tf_odom_map.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
+#print(tf_odom_map)
 
-#Izbacivanje iz planova i footprinta zasad nije implementirano, jer nakon izbacivanja redova iz datafejmova indeksi zadrzavaju svoje vrijednosti,
-# tako da se dalje mogu plan i footprint instance na isti nacin indeksirati
+# Delete entries with 'None' frame from tf_map_odom
+tf_map_odom.drop(index=tf_map_odom.index[:numOfFirstRowsToDelete], axis=0, inplace=True)
+#print(tf_map_odom)
+
+
+# Deletion from plans and footprints has not been implemented yet, because after deleting rows from datafames, indexes retain their values,
+# so that further plan and footprint instances can be indexed in the same way.
 
 
 # Dataset creation
-
 X_train = []
 X_test = []
 
-# Ako ce se koristiti LIME tabular
-# ' cmd_vel_ang_z' - nekad u buducnosti ispraviti ovaj razmak - izbrisati ga
+# If the LIME tabular is to be used
+# ' cmd_vel_ang_z' - sometime in the future to correct this gap - delete it
 if explanationMode == 'tabular':
     from lime_explainer import DatasetCreator
 
-    # Biranje ulaza za explanation algorithm
-    X = odom.iloc[:,6:8] # ulaz za objasnjenje su odometrijske brzine
+    # Select input for explanation algorithm
+    X = odom.iloc[:,6:8] # input for explanation are odometry velocities
     #print(X)
 
-
     if mode == 'regression':
-
         import numpy as np
 
         # regression
-        # Biranje izlaza za explanation algorithm
-        index_output_class = 1 # [0] - komandna linearna brzina, [1] - komandna ugaona brzina
+        # Selecting the output for the explanation algorithm
+        index_output_class = 1 # [0] - command linear velocity, [1] - command angular velocity
         y = cmd_vel.iloc[:,index_output_class:index_output_class+1]
         #print(y)        
-        output_class_name = y.columns.values[0] # 'cmd_vel_lin_x' - [0] or ' cmd_vel_ang_z' - [0]
+        output_class_name = y.columns.values[0]
 
     elif (mode == 'classification') & (one_hot_encoding == False):
-
         import numpy as np
         
         # classification        
-        # left-right-straight logika
+        # left-right-straight logic
         conditions = [
             (cmd_vel[' cmd_vel_ang_z'] >= 0),
             (cmd_vel[' cmd_vel_ang_z'] < 0)
@@ -110,24 +105,23 @@ if explanationMode == 'tabular':
 
         cmd_vel['direction'] = np.select(conditions, values)
         
-        # Biranje izlaza za explanation algorithm
+        # Selecting the output for the explanation algorithm
         index_output_class = 2 # [2] - direction
-        y = cmd_vel.iloc[:,index_output_class:index_output_class+1] # izlaz za objasnjavanje je direction
+        y = cmd_vel.iloc[:,index_output_class:index_output_class+1] # the output for explanation is direction
         #print(y)
-        output_class_name = y.columns.values[0] # 'direction' - [0]
+        output_class_name = y.columns.values[0]
 
     elif (mode == 'classification') & (one_hot_encoding == True):
-
         import numpy as np
         
         # random forest classification - one-hot encoding        
-        # left-right-straight logika
+        # left-right-straight logic
         conditions = [
             (cmd_vel[' cmd_vel_ang_z'] >= 0),
             (cmd_vel[' cmd_vel_ang_z'] < 0)
             ]
 
-        # one-hot left-right kodiranje
+        # one-hot left-right coding
         valuesLeft = [1.0, 0.0]
 
         cmd_vel['left'] = np.select(conditions, valuesLeft)
@@ -136,9 +130,9 @@ if explanationMode == 'tabular':
 
         cmd_vel['right'] = np.select(conditions, valuesRight)
         
-        # Biranje izlaza za explanation algorithm
-        index_output_class = 2 # 'left' & 'right'
-        y = cmd_vel.iloc[:,index_output_class:index_output_class+2] # izlaz za objasnjavanje je direction, odnosno left, right and straight one-hot enkodirano
+        # Selecting the output for the explanation algorithm
+        index_output_class = 2 # [2] - 'left', [3] - 'right'
+        y = cmd_vel.iloc[:,index_output_class:index_output_class+2] # the explanation output is direction, i.e. left, right and straight one-hot encoded
         #print(y)
         output_class_name = y.columns.values[0] # 'left' - [0] or 'right' - [1]
 
@@ -149,11 +143,11 @@ if explanationMode == 'tabular':
     
     slice_ratio = 0.01 # very small slice_ratio ensures that almost all data is put in X_train
     
-    X_train, X_test, y_train, y_test = DatasetCreator.split_test_train(X, y, slice_ratio, randomNum) # Imena redova (indexi) i nakon mijesanja ostaju ocuvana - very good
+    X_train, X_test, y_train, y_test = DatasetCreator.split_test_train(X, y, slice_ratio, randomNum) # Row names (indexes) remain preserved even after mixing - very good
 
 
 '''  
-    # Osiguranje da ovi podaci budu Datafrejmovi, ali nije potrebno, jer vec jesu. Nek stoji radi ispisa pri mogucem debuggingu
+    # Ensuring that this data is Dataframes, but not necessary, because they already are. Let it stand for printing while possibly debugging
     import pandas as pd
     X_train = pd.DataFrame(X_train)
     print(X_train)
@@ -167,7 +161,7 @@ if explanationMode == 'tabular':
 '''
 
 
-# Ako ce se koristiti LIME image or LIME tabular sa costmap ulazom
+# If a LIME image or LIME tabular with costmap input will be used
 if (explanationMode == 'image') | (explanationMode == 'tabular_costmap'):
     import pandas as pd
     X_train = pd.DataFrame()
@@ -176,29 +170,29 @@ if (explanationMode == 'image') | (explanationMode == 'tabular_costmap'):
     y_test = pd.DataFrame()
     
 
-# Biranje expID
-# choose/generate expID - redni broj reda u X_test ili X_train iz kojeg se vadi index
+# Choosing expID
+# choose/generate expID - ordinal number of the row in X_test or X_train from which the index is extracted
 if explanationMode == 'tabular':
-    # odabir po zelji - deterministicki
+    # optional selection - deterministic
     expID = 86
 
     # Dataset1:
     # Dataset2:
     # Dataset3:
     
-    # random odabir
+    # random selection
     #import random
     #expID = random.randint(0, X_train.shape[0]) # expID se trazi iz X_train
 
 else:
-    # odabir po zelji - deterministicki
+    # optional selection - deterministic
     expID = 71
 
     # Dataset1:
     # Dataset2:
     # Dataset3: #48 #92
     
-    # random odabir
+    # random selection
     #import random
     #expID = random.randint(0, local_costmap_info.shape[0]) # expID se trazi iz local_costmap_info
     
