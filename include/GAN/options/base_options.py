c6d5d20498ed0160a20816e417755f1f6f9d4622
ah96 <amarhalilovic96@gmail.com>
2022-02-19 13:33:13 +0100

    .

--- include/GAN/options/base_options.py
+++ include/GAN/options/base_options.py
@@ -23,7 +23,7 @@ class BaseOptions():
         parser.add_argument('--dataroot', required=False, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         parser.add_argument('--name', type=str, default='lime_pix2pix', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        parser.add_argument('--checkpoints_dir', type=str, default='/home/amar/amar_ws/src/lime_explainer/include/GAN/checkpoints', help='models are saved here')
+        parser.add_argument('--checkpoints_dir', type=str, default='/home/amar/amar_ws/src/navigation_explainer/include/GAN/checkpoints', help='models are saved here')
         # model parameters
         parser.add_argument('--model', type=str, default='pix2pix', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
         parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
