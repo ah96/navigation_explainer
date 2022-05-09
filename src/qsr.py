c6326ef3b0532ce436ed39caa1ffea35640dd167
ah96 <amarhalilovic96@gmail.com>
2022-05-09 07:26:56 +0200

    .

--- src/qsr.py
+++ src/qsr.py
@@ -197,6 +197,161 @@ class qsr():
         # used for deriving NLP annotations
         self.tpcc_dict_inv = {v: k for k, v in self.tpcc_dict.items()}
 
+    # define QSR calculus
+    def defineRobotQsrCalculus(self, qsr_choice):
+        if qsr_choice == 0:
+            # left -- right dichotomy in a relative refence system
+            # from 'moratz2008qualitative'
+            # used for getting semantic costmap
+            self.tpcc_dict_robot = {
+                'left': 0,
+                'right': 1
+            }
+
+        elif qsr_choice == 1:
+            # single cross calculus from 'moratz2008qualitative'
+            # used for getting semantic costmap
+            self.tpcc_dict_robot = {
+                'left/front': 0,
+                'right/front': 1,
+                'left/back': 2,
+                'right/back': 3
+            }
+
+        elif qsr_choice == 2:
+            # TPCC reference system
+            # my modified version from 'moratz2008qualitative'
+            # used for getting semantic costmap
+            if self.R == 0:
+                self.tpcc_dict_robot = {
+                    'sb': 0,
+                    'lb': 1,
+                    'bl': 2,
+                    'sl': 3,
+                    'fl': 4,
+                    'lf': 5,
+                    'sf': 6,
+                    'rf': 7,
+                    'fr': 8,
+                    'sr': 9,
+                    'br': 10,
+                    'rb': 11
+                }
+            else:
+                self.tpcc_dict_robot = {
+                    'csb': 0,
+                    'dsb': 1,
+                    'clb': 2,
+                    'dlb': 3,
+                    'cbl': 4,
+                    'dbl': 5,
+                    'csl': 6,
+                    'dsl': 7,
+                    'cfl': 8,
+                    'dfl': 9,
+                    'clf': 10,
+                    'dlf': 11,
+                    'csf': 12,
+                    'dsf': 13,
+                    'crf': 14,
+                    'drf': 15,
+                    'cfr': 16,
+                    'dfr': 17,
+                    'csr': 18,
+                    'dsr': 19,
+                    'cbr': 20,
+                    'dbr': 21,
+                    'crb': 22,                    
+                    'drb': 23
+                }
+
+                self.comb_table_robot = [
+                    [[0,1],[0,1],[2,3],[2,3],[2,3],[2,3,4,5],[2,3],[2,3,4,5],[2,3,4,5],[4,5,6,7,8,9],[2,4],[6,8,9,10,11],[0],[12,13]],
+                    [[1],[1],[3],[3],[3],[3,5],[3],[3,5],[2,3,4,5],[4,5,7,9],[2,3,4,5],[4,5,6,7,8,9,10,11],[0,1],[12,13]],
+                    [[2,3],[2,3],[2,3,4,5],[2,3,4,5],[2,3,4,5],[2,3,4,5,6,7,8,9],[2,3,4,5],[4,5,6,7,8,9],[2,3,4,5,6,7,8,9],[4,5,6,7,8,9,10,11],[2,4,6,8],[4,6,8,9,10,11,12,13,14,15],[2],[14,15]],
+                    [[3],[3],[3,5],[3,5],[3,5],[3,5,7,9],[3,5],[5,7,9],[2,3,4,5,6,7,8,9],[4,5,6,7,8,9,11],[2,3,4,5,6,7,8,9],[4,5,6,7,8,9,10,11,12,13,14,15],[2,3],[14,15]],
+                    [[4,5],[4,5],[4,5,6,7,8,9],[4,5,6,7,8,9],[4,5,6,7,8,9],[4,5,6,7,8,9,10,11],[4,5,6,7,8,9],[8,9,10,11],[4,5,6,7,8,9,10,11],[8,9,10,11,12,13,14,15],[4,6,8,10],[8,10,11,12,13,14,15,17],[4],[16,17]],
+                    [[5],[5],[5,7,9],[5,7,9],[5,7,9],[5,7,9,11],[5,7,9],[9,11],[4,5,6,7,8,9,10,11],[8,9,10,11,13,15],[4,5,6,7,8,9,10,11],[9,10,11,12,13,14,15,16,17],[4,5],[16,17]],
+                    [[6,7],[6,7],[8,9],[8,9],[8,9],[8,9,10,11],[8,9],[10,11],[8,9,10,11],[10,11,12,13,14,15],[8,10],[10,12,14,15,16,17],[6],[18,19]],
+                    [[7],[7],[9],[9],[9],[9,11],[9],[11],[8,9,10,11],[10,11,13,15],[8,9,10,11],[10,11,12,13,14,15,18],[6,7],[18,19]],
+                    [[8,9],[8,9],[8,9,10,11],[8,9,10,11],[8,9,10,11],[8,9,10,11,12,13,14,15],[8,9,10,11],[10,11,12,13,14,15],[8,9,10,11,12,13,14,15],[10,11,12,13,14,15,16,17],[8,10,12,14],[10,12,14,15,16,17,18,19,20,21],[8],[20,21]],
+                    [[9],[9],[9,11],[9,11],[9,11],[9,11,13,15],[9,11],[11,13,15],[8,9,10,11,12,13,14,15],[10,11,12,13,14,15,17],[8,9,10,11,12,13,14,15],[10,11,12,13,14,15,16,17,18,19,20,21],[8,9],[20,21]],
+                    [[10,11],[10,11],[10,11,12,13,14,15],[10,11,12,13,14,15],[10,11,12,13,14,15],[10,11,12,13,14,15,16,17],[10,11,12,13,14,15],[12,13,14,15,16,17],[10,11,12,13,14,15,16,17],[15,16,17,18,19,20,21],[10,12,14,16],[14,16,17,18,19,20,21,22,23],[10],[22,23]],
+                    [[11],[11],[11,13,15],[11,13,15],[11,13,15],[11,13,15,17],[11,13,15],[13,15,17],[10,11,12,13,14,15,16,17],[14,15,16,17,19,21],[10,11,12,13,14,15,16,17],[14,15,16,17,18,19,20,21,22,23],[10,11],[22,23]],
+                    [[12,13],[12,13],[14,15],[14,15],[14,15],[14,15,16,17],[14,15],[14,15,16,17],[14,15,16,17],[16,17,18,19,20,21],[14,16],[16,18,20,21,22,23],[12],[0,1]],
+                    [[13],[13],[15],[15],[15],[15,17],[15],[15,17],[14,15,16,17],[16,17,19,21],[14,15,16,17],[16,17,18,19,20,21,22,23],[12,13],[0,1]],
+                    [[14,15],[14,15],[14,15,16,17],[14,15,16,17],[14,15,16,17],[15,16,17,18,19,20,21],[14,15,16,17],[16,17,18,19,20,21],[14,15,16,17,18,19,20,21],[16,17,18,19,20,21,22,23],[14,16,18,20],[0,1,2,3,16,18,20,21,22,23],[14],[2,3]],
+                    [[15],[15],[15,17],[15,17],[15,17],[15,17,19,21],[15,17],[17,19,21],[14,15,16,17,18,19,20,21],[16,17,18,19,20,21,23],[14,15,16,17,18,19,20,21],[0,1,2,3,16,17,18,19,20,21,22,23],[14,15],[2,3]],
+                    [[16,17],[16,17],[16,17,18,19,20,21],[16,17,18,19,20,21],[16,17,18,19,20,21],[16,17,18,19,20,21,22,23],[16,17,18,19,20,21],[20,21,22,23],[16,17,18,19,20,21,22,23],[0,1,2,3,20,21,22,23],[16,18,20,22],[0,1,2,3,4,5,20,22,23],[16],[4,5]],
+                    [[17],[17],[17,19,21],[17,19,21],[17,19,21],[17,19,21,23],[17,19,21],[21,23],[16,17,18,19,20,21,22,23],[1,3,20,21,22,23],[16,17,18,19,20,21,22,23],[0,1,2,3,4,5,20,21,22,23],[16,17],[4,5]],
+                    [[18,19],[18,19],[20,21],[20,21],[20,21],[20,21,22,23],[20,21],[22,23],[20,21,22,23],[0,1,2,3,22,23],[20,22],[0,2,3,4,5,22],[18],[6,7]],
+                    [[19],[19],[21],[21],[21],[21,23],[21],[23],[20,21,22,23],[1,3,22,23],[20,21,22,23],[0,1,2,3,4,5,22,23],[18,19],[6,7]],
+                    [[20,21],[20,21],[20,21,22,23],[20,21,22,23],[20,21,22,23],[0,1,2,3,20,21,22,23],[20,21,22,23],[0,1,2,3,22,23],[0,1,2,3,20,21,22,23],[0,1,2,3,4,5,22,23],[0,2,20,22],[0,2,3,4,5,6,7,8,9,22],[20],[8,9]],
+                    [[21],[21],[21,23],[21,23],[21,23],[1,3,21,23],[21,23],[1,3,23],[0,1,2,3,20,21,22,23],[0,1,2,3,5,22],[0,1,2,3,20,21,22,23],[0,1,2,3,4,5,6,7,8,9,23],[20,21],[8,9]],
+                    [[22,23],[22,23],[0,1,2,3,22,23],[0,1,2,3,22,23],[0,1,2,3,22,23],[0,1,2,3,4,5,22,23],[0,1,2,3,22,23],[0,1,2,3,4,5],[0,1,2,3,4,5,22,23],[2,3,4,5,6,7,8,9],[0,2,4,22],[2,4,5,6,7,8,9,10,11],[22],[10,11]],
+                    [[23],[23],[1,3,23],[1,3,23],[1,3,23],[1,3,5,23],[1,3,23],[1,3,5],[0,1,2,3,4,5,22,23],[2,3,4,5,7,9],[0,1,2,3,4,5,22,23],[3,4,5,6,7,8,9,10,11],[22,23],[10,11]]
+                    ]
+
+                self.perm_dict_robot = {
+                    'ID':0,
+                    'INV':1,
+                    'SC':2,
+                    'SCI':3,
+                    'HM':4,
+                    'HMI':5
+                }
+
+                self.per_table_robot = [
+                    [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13]],
+                    [[13],[13],[15],[15],[15],[15,17],[15],[15,17],[14,15,16,17],[16,17,19,21],[14,16],[16,18,20,21,22,23],[12],[0,1]],
+                    [[12],[12],[14,16],[14,16],[14,16,18],[14],[16,18],[14],[16,17,18,20],[14,15,16,17],[17,19,20,21,22,23],[15,17],[0,1],[15]],
+                    [[12],[12],[10],[10],[10,11],[8,9,10],[10,11],[8,9,10],[8,9,10,11],[4,6,8,9],[9,11],[2,3,4,5,7,9],[13],[2,3]],
+                    [[13],[13],[11],[11],[9,11],[11],[9],[11],[5,7,8,9],[8,9,10,11],[2,3,4,5,6,8],[8,10],[2,3],[0,1]],
+                    [[1],[0,1],[22,23],[22,23],[20,21],[20,21,22],[21],[20,21],[16,17,19,21],[16,17,18,20],[14,15,17],[14,15,17],[15],[14,15]]        
+                ]
+
+        elif qsr_choice == 3:
+            # A model [(Herrmann, 1990),(Hernandez, 1994)] from 'moratz2002spatial'
+            # used for getting semantic costmap
+            self.tpcc_dict_robot = {
+                'front': 0,
+                'left': 1,
+                'back': 2,
+                'right': 3
+            }
+
+        elif qsr_choice == 4:
+            # Model for combined expressions from 'moratz2002spatial'
+            # used for getting semantic costmap
+            self.tpcc_dict_robot = {
+                'left-front': 0,
+                'left-back': 1,
+                'right-front': 2,
+                'right-back': 3,
+                'straight-front': 4,
+                'exactly-left': 5,
+                'straight-back': 6,
+                'exactly-right': 7
+            }    
+
+        # used for deriving NLP annotations
+        self.tpcc_dict_inv_robot = {v: k for k, v in self.tpcc_dict.items()}
+    
+    def getRobotValue(self, angle):
+        value = ''    
+
+        if qsr_choice_ == 1:
+            if 0 <= angle < self.PI/2:
+                value += 'right/back'
+            elif self.PI/2 <= angle <= self.PI:
+                value += 'right/front'
+            elif -self.PI/2 <= angle < 0:
+                value += 'left/back'
+            elif -self.PI <= angle < -self.PI/2:
+                value += 'left/front'
+
+        return value
+
     # convert orientation quaternion to euler angles
     def quaternion_to_euler(self, x, y, z, w):
         # roll (x-axis rotation)
@@ -491,57 +646,76 @@ class qsr():
             # print QSR triples every I=100 iterations
             #print('\n\n')
             #print(self.printTriples(triples))
+
+            # Textual explanations based on LIME
+            N_segments = len(self.lime_exp)
+            if N_segments > 0:
+                N_objects_in_lc = len(self.objects_in_lc_names)
+                print('\n\n')
+                #print('self.lime_exp = ', self.lime_exp)
+                #print('N_segments = ', N_segments)
+                #print('N_objects_in_lc = ', N_objects_in_lc)
+                #print('self.objects_in_lc_names = ', self.objects_in_lc_names)
+
+                #distances = [[0.0]*N_objects_in_lc]*N_segments # N_segments x N_objects_in_lc
+                distances = []
+                objects_min_dist = []
+                segments_min_dist = []
+
+                for i in range(0, N_segments):
+                    distances.append([])
+                    for j in range(0, N_objects_in_lc):
+                        d_x = self.objects_in_lc_positions[j][0] - self.lime_exp[i][0]
+                        d_y = self.objects_in_lc_positions[j][1] - self.lime_exp[i][1]
+                        dist = math.sqrt((d_x)**2+(d_y)**2)
+                        distances[i].append(dist)
+                    segments_min_dist.append(min(distances[i]))
+                    #print('distances[' + str(i) + '] = ', distances[i])
+                    #print('min(distances[' + str(i) + ']) = ', min(distances[i]))    
+                    
+                #objects_min_dist = [min(distances[:][j]) for j in range(0, N_objects_in_lc)]    
+
+                #print('\ndistances = ', distances)
+                #print('\nobjects_min_dist = ', objects_min_dist)
+                #print('\nsegments_min_dist = ', segments_min_dist)
+
+                #sorted_indices_of_obj_min_dist = sorted(range(N_objects_in_lc), key = lambda k: objects_min_dist[k])
+                sorted_indices_of_seg_min_dist = sorted(range(N_segments), key = lambda k: segments_min_dist[k])
+                #print('sorted_indices_of_seg_min_dist = ', sorted_indices_of_seg_min_dist)
+
+                # match objects and lime coefficients
+                objects_coeff_pairs = dict()
+                used_names = [] 
+                for i in range(0, N_segments):
+                    idx = sorted_indices_of_seg_min_dist[i]
+                    obj_name_ = self.objects_in_lc_names[distances[idx].index(segments_min_dist[idx])]
+                    if obj_name_ in used_names:
+                        continue
+                    used_names.append(obj_name_)
+                    lime_coeff_ = self.lime_exp[idx][2]
+                    #print(obj_name_ + ' has weight ' + str(lime_coeff_))
+                    objects_coeff_pairs[obj_name_] = lime_coeff_
+
+                    # find triple
+                    name_idx = self.referents_names.index(obj_name_)
+                    d_x = self.referents_positions[name_idx][0] - self.RELATUM_pos[0]
+                    d_y = self.referents_positions[name_idx][1] - self.RELATUM_pos[1]
+                    r = math.sqrt(d_x**2+d_y**2)
+                    angle_ = np.arctan2(d_y, d_x)
+                    angle = angle_ - self.angle_ref
+                    if angle > self.PI:
+                        angle -= 2*self.PI
+                    elif angle <= -self.PI:
+                        angle += 2*self.PI
+                    value = self.getValue(r, angle)    
+                    print(self.ORIGIN_name + ',' + self.RELATUM_name + ' ' + value + ' ' + obj_name_ + ' with lime coefficient of ' + str(lime_coeff_))
+
+                    value = self.getRobotValue(angle)
+                    print(obj_name_ + ' is ' + value + ' from the ' + self.RELATUM_name + ' with lime coefficient of ' + str(lime_coeff_))
         #'''
 
         # do QSR reasoning with 4 points
         #reason(states_msg)
-
-        #transf = tfBuffer.lookup_transform('odom', 'map', rospy.Time())
-        #p = np.array([px, py, 0.0])
-        #t = np.asarray([transf.transform.translation.x,transf.transform.translation.y,transf.transform.translation.z])
-        #r = R.from_quat([transf.transform.rotation.x,transf.transform.rotation.y,transf.transform.rotation.z,transf.transform.rotation.w])
-        #r_ = np.asarray(r.as_matrix())
-        #pnew = p.dot(r_) + t
-
-        # Textual explanations based on LIME
-        N_segments = len(self.lime_exp)
-        if N_segments > 0:
-            N_objects_in_lc = len(self.objects_in_lc_names)
-            print('\n\n')
-            print('self.lime_exp = ', self.lime_exp)
-            print('N_segments = ', N_segments)
-            print('N_objects_in_lc = ', N_objects_in_lc)
-            print('self.objects_in_lc_names = ', self.objects_in_lc_names)
-
-            #distances = [[0.0]*N_objects_in_lc]*N_segments # N_segments x N_objects_in_lc
-            distances = []
-            objects_min_dist = []
-            segments_min_dist = []
-
-            for i in range(0, N_segments):
-                distances.append([])
-                for j in range(0, N_objects_in_lc):
-                    d_x = self.objects_in_lc_positions[j][0] - self.lime_exp[i][0]
-                    d_y = self.objects_in_lc_positions[j][1] - self.lime_exp[i][1]
-                    dist = math.sqrt((d_x)**2+(d_y)**2)
-                    distances[i].append(dist)
-                segments_min_dist.append(min(distances[i]))
-                print('distances[' + str(i) + '] = ', distances[i])
-                print('min(distances[' + str(i) + ']) = ', min(distances[i]))    
-                
-            #objects_min_dist = [min(distances[:][j]) for j in range(0, N_objects_in_lc)]    
-
-            print('\ndistances = ', distances)
-            #print('\nobjects_min_dist = ', objects_min_dist)
-            print('\nsegments_min_dist = ', segments_min_dist)
-
-            #sorted_indices_of_obj_min_dist = sorted(range(N_objects_in_lc), key = lambda k: objects_min_dist[k])
-            sorted_indices_of_seg_min_dist = sorted(range(N_segments), key = lambda k: segments_min_dist[k])
-            print('sorted_indices_of_seg_min_dist = ', sorted_indices_of_seg_min_dist)
-
-            for i in range(0, N_segments):
-                idx = sorted_indices_of_seg_min_dist[i]
-                print(self.objects_in_lc_names[distances[idx].index(segments_min_dist[idx])] + ' has weight ' + str(self.lime_exp[idx][2]))
 
     def getValue(self, r, angle):
         value = ''    
@@ -657,6 +831,9 @@ qsr_obj = qsr()
 
 qsr_choice = 2 
 qsr_obj.defineQsrCalculus(qsr_choice)
+
+qsr_choice_ = 1 
+qsr_obj.defineRobotQsrCalculus(qsr_choice_)
 
 #tfBuffer = tf2_ros.Buffer()
 
